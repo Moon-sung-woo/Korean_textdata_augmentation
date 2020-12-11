eb42bcd95feca86729f551f31c2d2ac19625095f
msw <tjddn3322@naver.com>
2020-12-12 00:27:10 +0900

    n_gram_합치는 코드 추가

--- make_dataset.py
+++ make_dataset.py
@@ -4,6 +4,7 @@ import openpyxl
 import pandas as pd
 import glob
 import os
+import csv
 #from g2pk import G2p
 
 #g2p = G2p()
@@ -41,9 +42,7 @@ def sum_sheet():
     print('######################################################################')
     print(test_df)
 
-def sum_csv():
-    input_file = r'data' # csv파일들이 있는 디렉토리 위치
-    output_file = r'output_data/clean_g2pk_with_EDA_asr_v4.csv'
+def sum_csv(input_file, output_file):
 
     allFile_list = glob.glob(os.path.join(input_file, '*')) #sum_으로 되어있는 파일들을 모은다.
     print(allFile_list)
@@ -73,5 +72,46 @@ def add_column_name():
     df.columns = ['Column1', 'Column2']  # 컬럼명 추가해 주는 부분
     df.to_csv(output_file, index=False, encoding='utf-8')
 
-sum_csv()
+def mix_data(mix_num):
+    input_file = 'data/clean_g2pk_EDA_asr_v5.csv'
+    output_file = 'mix'
+
+    df = pd.read_csv(input_file)
+    df.columns = ['Column1', 'Column2'] #컬럼명 추가해 주는 부분
+
+    for i in range(5):
+        df_s = df.loc[df['Column2'] == i, :]
+
+        df_s = df_s.sample(frac=1).reset_index(drop=True) # shuffle
+
+        f = open(output_file + '/mix_data_{}.csv'.format(i), 'w', encoding='utf_8')
+        wr = csv.writer(f)
+
+        for j in range(len(df_s) - mix_num + 1):
+            n_gram = df_s.iloc[j:j+mix_num, 0]
+            new_script = []
+
+            for n_script in n_gram:
+                print(n_script)
+                new_script.append(n_script)
+
+            print(' '.join(new_script))
+            result = ' '.join(new_script)
+            wr.writerow([result, i])
+
+        f.close()
+
+    sum_input_path = r'mix' # csv파일들이 있는 디렉토리 위치
+    output_path = r'output_data/clean_g2pk_EDA_asr_{}gram_v5.csv'.format(mix_num)
+    sum_csv(sum_input_path, output_file=output_path)
+
+
+# mix_data(mix_num=7)
+
+
+input_file = r'data'
+output_file = r'output_data/sum67_gram_clean_g2pk_EDA_asr_v7.csv'
+
+sum_csv(input_file=input_file, output_file=output_file)
+
 #add_column_name()
